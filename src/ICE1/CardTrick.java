98b3b5815b2c6731e6453bcc90d70d8e00303a2d
muskan49muskan <133687185+muskan49muskan@users.noreply.github.com>
2023-05-31 20:30:02 -0400

    Update CardTrick.java

--- src/ICE1/CardTrick.java
+++ src/ICE1/CardTrick.java
@@ -5,15 +5,6 @@
 
 package ICE1;
 
-/**A class that fills a magic hand of 7 cards with random Card Objects
- * and then asks the user to pick a card and searches the array of cards
- * for the match to the user's card. To be used as starting code in ICE 1
- * 991707407
- * @author muskan muskan
- * 
- * 
- */
-import card.Card;
 import java.util.Scanner;
 public class CardTrick {
 
